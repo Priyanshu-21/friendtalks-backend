6199789bbb3cc6e5c364d33a9902fd9e8c028825
Priyanshu Mishra <pmfanofms21@gmail.com>
2024-10-30 11:50:07 +0530

    Server Connection

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
+import express from "express"; 
+import cors from "cors"
+import cookieParser from "cookie-parser";
+const app = express(); 
 
+//Enabling every client reqeust to server (cors)
+app.use(cors({
+    origin: process.env.CORS_ORIGIN, 
+    credentials: true, 
+})); 
+
+//Limit the json request to be only 16kb size
+app.use(express.json({  limit: "16kb" })); 
+
+//URL encoded with space structure should be 
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); 
+
+//Store images or files in static on server
+app.use(express.static("public")); 
+
+//Use the cookie-parser (Middleware)
+app.use(cookieParser()); 
+
+
+
+export default app; 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-import dotenv from "dotenv"; 
+import dotenv from "dotenv";
 import dbConnect from "./db/dbConnect.js";
-dotenv.config({ path: "./env" }); 
+import app from "./app.js";
+dotenv.config({ path: "./env" });
 
-dbConnect();
+// connection to Database will return promisified values
+// First connnection to Db then connection to servers
+dbConnect()
+  .then(() => {
+    app.listen(process.env.PORT || 8080, () => {
+      console.log(`Server is running on Port: ${process.env.PORT}`);
+    });
+
+    //when Server connection is failing then
+    app.on("error", (error) => {
+      console.error("Server failed to connect: ", error);
+      throw error; 
+    });
+  })
+  .catch((error) => {
+    console.error("MONGODB connection failed: ", error);
+  });
